acd631c0da1250631b1502bf8fc52690239b6282
yand1960 <yand1960@gmail.com>
2021-09-28 12:01:02 +0300

    YA

--- JA03_Threads/src/my/threads/Client.java
+++ JA03_Threads/src/my/threads/Client.java
@@ -4,17 +4,73 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 public class Client {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        Использование Executors и Futures
+//        (главное - в классе SlowMeter)
 
-//        Асинхронный код написать не проблема,
-//        но рещение последующих проблем синхронизации
-//        (в данном случае, проблема вывода покзааний
-//        в правильном порядке) может оказазаться непростой
+        SlowMeter meter = new SlowMeter();
+        List<Future<Integer>> results = new ArrayList<>();
+
+//        Асинхронные запуски при таком подходе весьма изящны
+        for(int n = 1; n < 10; n++) {
+            results.add(meter.measureAsync(n));
+        }
+
+        for(Future<Integer> f: results) {
+            // вызов get() ждет получения результатов
+            // и возвращает их сразу, если они уже готовы
+            System.out.println(f.get());
+        }
+
+        //Executor'ы не заканчивают работу автоматом,
+        // когда заканивает работу приложение,
+        // поэтому их нужно останавливать явно
+        meter.shutdown();
+    }
+
+    public static void main4(String[] args) throws InterruptedException {
+
+//      Нюансы и варианты
+
+        SlowMeter meter = new SlowMeter();
+        ConcurrentHashMap<Integer,Integer> results
+                                = new ConcurrentHashMap();
+        // Иметь в пуле ссылки на все порожденные потоки
+        // - это обычная практика
+        List<Thread> pool = new ArrayList<>();
+
+        for(int n = 1; n < 10; n++) {
+            final int m = n;
+            Thread thread = new Thread(() -> {
+                int result = meter.measure(m);
+//              Используем потобезопасную коллекцию
+                results.put(m, result);
+            });
+            pool.add(thread);
+            thread.start();
+        }
+
+        //Универсальный способ
+        // определить окончание работы всех потоков
+        for(Thread t: pool) {
+            t.join();
+        }
+
+        System.out.println(results);
+
+    }
+
+    public static void main3(String[] args) throws InterruptedException {
+
+//      Удовлетворительное решение
 
         SlowMeter meter = new SlowMeter();
         int[] n = {1,2,3,4,5,6,7,8,9};
@@ -34,7 +90,18 @@ public class Client {
         });
 
 //        Дешево и сердито
-        Thread.sleep(3500);
+//        Thread.sleep(3500);
+
+        //Относительно хороший спопособ
+        // определить окончание работы всех потоков
+        boolean all_done = false;
+        while(!all_done) {
+            lock.lock();
+            all_done = (results.size() == 9);
+            lock.unlock();
+            Thread.sleep(100);
+        }
+
 
         //Задача: отсортируйте результаты по убыванию,
         //используя современные методы
--- JA03_Threads/src/my/threads/SlowMeter.java
+++ JA03_Threads/src/my/threads/SlowMeter.java
@@ -1,6 +1,18 @@
 package my.threads;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class SlowMeter {
+
+    // Executorы снимают с девлопера заботу
+    // о явном контроле над потоками b и обеспечивают
+    // синтаксически изящные мтетоды написания асинхронного кода
+    private ExecutorService executor
+            = Executors.newFixedThreadPool(20);
+
     public int measure(int n) {
         try {
             Thread.sleep(3000);
@@ -9,5 +21,16 @@ public class SlowMeter {
         }
         int result = 10 * n;
         return result;
+    }
+
+//    "Асинхронная обертка" над синхронным методом
+//    Future - это родственник Task в С# и Promise в JS
+    public Future<Integer> measureAsync(int n) {
+        return executor.submit(() -> measure(n));
+    }
+
+
+    public void shutdown() {
+        executor.shutdown();
     }
 }
