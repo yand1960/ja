e53407c375760849de5da9419cbd3034c67f8e2a
yand1960 <yand1960@gmail.com>
2021-09-29 15:53:51 +0300

    YA

--- JA05_Network/src/main/java/my/web/HttpDemo.java
+++ JA05_Network/src/main/java/my/web/HttpDemo.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpDemo {
     public static void main(String[] args) throws IOException {
@@ -19,14 +21,17 @@ public class HttpDemo {
             result += reader.readLine() + "\n";
         }
 
-        int position = result.indexOf("USD");
-        result = result.substring(position,position + 100);
-        String pattern = "mono-num\">";
-        int position1 = result.indexOf(pattern) + pattern.length();
-        int position2 = result.indexOf("₽");
-        result = result.substring(position1, position2);
-        System.out.println(result);
+//        int position = result.indexOf("USD");
+//        result = result.substring(position,position + 100);
+//        String pattern = "mono-num\">";
+//        int position1 = result.indexOf(pattern) + pattern.length();
+//        int position2 = result.indexOf("₽");
+//        result = result.substring(position1, position2);
+//        System.out.println(result);
 
-
+        String pattern = "USD[\\S\\s]+?mono-num\">(\\S+)";
+        Matcher matcher = Pattern.compile(pattern).matcher(result);
+        matcher.find();
+        System.out.println(matcher.group(1));
     }
 }
