1127b962f67d345caea6a77f6f3d4b7082b0c4d7
yand1960 <yand1960@gmail.com>
2021-10-04 10:46:48 +0300

    YA

---
+++ SP01_DI/src/main/java/my/animals1/Animal.java
@@ -0,0 +1,7 @@
+package my.animals1;
+
+public interface Animal {
+    void eat();
+    String getVoice();
+    void setVoice(String value);
+}
---
+++ SP01_DI/src/main/java/my/animals1/Client.java
@@ -0,0 +1,11 @@
+package my.animals1;
+
+public class Client {
+    public static void main(String[] args) {
+        // При POJO не удается избежать тесных зависимостей
+//        Animal a1 = new Cat();
+//        a1.eat();
+
+
+    }
+}
