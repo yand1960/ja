8aac1aa50e626e4fb5e3afcb2152ab54805ca710
yand1960 <yand1960@gmail.com>
2021-10-06 16:45:23 +0300

    YA

--- SP05_Boot_MVC_Security/src/main/java/my/boot/MyMvcController.java
+++ SP05_Boot_MVC_Security/src/main/java/my/boot/MyMvcController.java
@@ -1,6 +1,5 @@
 package my.boot;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
